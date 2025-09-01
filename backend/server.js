3790eef18287db758533245c4a0a9e7ffa509ad1
Sanam-Shrestha04 <sthasanam067@gmail.com>
2025-09-01 23:02:07 +0545

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,10 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/reports", reportRoutes);
 app.use("/uploads", express.static("uploads"));
 
+app.get("/", (req, res) => {
+  res.send("TaskForge backend is live 🚀");
+});
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
